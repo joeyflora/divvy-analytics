59f71e4d0a61cb6392bd606f9a29e14de2348a18
jflora <joey.flora@gmail.com>
2021-02-23 09:49:46 -0700

    Create Map Filter Component
    Create Map Filter Component

---
+++ divvy-api/src/components/MapFilters.js
@@ -0,0 +1,34 @@
+import react from "react";
+
+const handleDockFilter = (n) =>{
+    
+}
+
+const MapFilters = () =>{
+    return(
+        // <div className="dropdown">
+        //     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        //         Dropdown button
+        //     </button>
+        //     <div className="dropdown-menu">
+        //         {/* <form className="px-4 py-3">
+        //             <div className="form-group">
+                        
+        //                 <input type="email" className="form-control" id="exampleDropdownFormEmail1" placeholder="email@example.com"/>
+        //             </div>
+        //             <div className="form-group">
+                        
+        //                 <input type="password" className="form-control" id="exampleDropdownFormPassword1" placeholder="Password"/>
+        //             </div>
+        //             <button type="submit" className="btn btn-primary">Sign in</button>
+        //         </form>
+        //         <div className="dropdown-divider"></div> */}
+        //         <a className="dropdown-item" href="#">New around here? Sign up</a>
+        //         <a className="dropdown-item" href="#">Forgot password?</a>
+        //     </div>
+        // </div>
+        <input type="number" ></input>
+    )
+}
+
+export default MapFilters;
